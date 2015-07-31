3d99777e4d37537899b57ba03f83623cb7500c32
Joy Chugh <joychugh@gmail.com>
2015-07-31 08:24:00 -0400

    Added Error Correction Added Tests. Work remains to make it friendly to streaming operations will use Guava Murmur3 hash instead of embedded one.

--- src/main/java/com/jchugh/hyperloglog/Cardinality.java
+++ src/main/java/com/jchugh/hyperloglog/Cardinality.java
@@ -1,10 +1,11 @@
 package com.jchugh.hyperloglog;
 
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import com.google.common.hash.HashingInputStream;
 import com.jchugh.hyperloglog.hashing.MurmurHash3;
 
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,18 +14,50 @@ import java.util.List;
  * When it will, There will be a test included.
  */
 public class Cardinality {
+    private final List<Byte> registers;
+    private final int logRegisterCount;
+    private final int registerCount;
+    private final double correctionConstant;
+    private int zeroValueRegisters;
 
-    private static final BigDecimal A = BigDecimal.valueOf(0.7213);
-    private static final BigDecimal ONE = BigDecimal.ONE;
-    private static final BigDecimal B = BigDecimal.valueOf(1.079);
+    public Cardinality(int logRegisterCount) {
+        if (!verifynumRegistersExponent(logRegisterCount)) {
+            throw new IllegalArgumentException("4 <= logRegisterCount <= 16");
+        }
+        this.registerCount = 1 << logRegisterCount;
+        this.registers = new ArrayList<>(this.registerCount);
+        this.logRegisterCount = logRegisterCount;
+        this.correctionConstant = getCorrectionConstant(this.registerCount);
+        zeroValueRegisters = 0;
+    }
+
+    public void offer(long input) {
+        int x = Hashing.murmur3_32().hashLong(input).asInt();
+        int shift = Integer.SIZE - logRegisterCount;
+        int j = x >>> shift;
+        int w = (x << logRegisterCount) | (1 << (logRegisterCount - 1));
+        registers.add((byte) Math.max(registers.get(j), Integer.numberOfLeadingZeros(w) + 1));
+
+    }
+
+    public void offer(String input) {
+
+    }
+
+    public void offer(byte[] input) {
+
+    }
+
 
     public static long getCardinality(final List<byte[]> input, final int numRegistersExponent) {
+        if (!verifynumRegistersExponent(numRegistersExponent)) {
+            throw new IllegalArgumentException("4 <= numRegistersExponent <= 16");
+        }
         final int numRegisters = 1 << numRegistersExponent;
-        // 0.7213/(1+1.079/2^numRegistersExponent for numRegisters > 128 ie. numRegistersExponent > 7)
-        // for simplicity of implementation we will assume numRegistersExponent > 7 and bits per register = 1 byte = 8
-        final BigDecimal correctionConstant = A.divide(ONE.add(B.divide(BigDecimal.valueOf(numRegisters), MathContext.DECIMAL32)), MathContext.DECIMAL32);
+        // for simplicity of implementation, bits per register = 1 byte = 8
+        final double correctionConstant = getCorrectionConstant(numRegisters);
         byte[] registers = new byte[numRegisters];
-        double zeroRegisterCount = 0.0;
+        int zeroRegisterCount = 0;
 
         for (byte[] v: input) {
             int x = MurmurHash3.murmurhash3_x86_32(v, 0, v.length, 0);
@@ -35,29 +68,48 @@ public class Cardinality {
             registers[j] = (byte) Math.max(registers[j], Integer.numberOfLeadingZeros(w) + 1);
         }
 
-        BigDecimal indicator = BigDecimal.ZERO;
+        double indicator = 0.0;
         for (int i=0; i < numRegisters; ++i) {
-            indicator = indicator.add(BigDecimal.valueOf(2).pow(-registers[i], MathContext.DECIMAL32));
+            // 2 ^ -register[i]
+            indicator += (1.0 / (1 << registers[i]));
             if (registers[i] == 0) zeroRegisterCount++;
         }
-        indicator = BigDecimal.ONE.divide(indicator, MathContext.DECIMAL32);
-        BigDecimal E = correctionConstant.multiply(BigDecimal.valueOf(numRegisters * numRegisters)).multiply(indicator);
+        indicator = 1/indicator;
+        double E = correctionConstant * numRegisters * numRegisters * indicator;
 
         return Cardinality.correct(E, numRegisters, zeroRegisterCount);
     }
 
-    private static long correct(BigDecimal E, int numOfRegisters, double emptyRegisters) {
-        if (E.longValue() <= (5.0/2.0) * numOfRegisters) {
-            if (emptyRegisters != 0) {
-                return Math.round(numOfRegisters * Math.log(numOfRegisters/emptyRegisters));
-            } else {
-                return E.longValue();
+    private static boolean verifynumRegistersExponent(int numRegisterExponent) {
+        if (numRegisterExponent < 4 || numRegisterExponent > 16) {
+            return false;
+        }
+        return true;
+    }
+
+    private static double getCorrectionConstant(int numOfRegisters) {
+        switch (numOfRegisters) {
+            case 16: return 0.673;
+            case 32: return 0.697;
+            case 64: return 0.709;
+            default: {
+                return 0.7213 / (1 + 1.079/numOfRegisters);
             }
         }
-        if (E.longValue() <= (1.0/30.0) * (1l << 32)) {
-            return E.longValue();
+    }
+
+    private static long correct(double E, int numOfRegisters, int emptyRegisters) {
+        if (E <= (5.0/2.0) * numOfRegisters) {
+            if (emptyRegisters != 0) {
+                return Math.round(numOfRegisters * Math.log(numOfRegisters/Double.valueOf(emptyRegisters)));
+            } else {
+                return Double.valueOf(E).longValue();
+            }
+        }
+        if (E <= (1.0/30.0) * (1l << 32)) {
+            return Double.valueOf(E).longValue();
         } else {
-            return Math.round(-(1l << 32) * Math.log(BigDecimal.ONE.subtract(E.divide(BigDecimal.valueOf(1l << 32), MathContext.DECIMAL32)).doubleValue()));
+            return Math.round(-(1l << 32) * Math.log(1 - E/(1l << 32)));
         }
     }
 }
