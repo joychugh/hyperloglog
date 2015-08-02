0ac962f0f5a58a14a62ba50f6270d02639334b6b
Joy Chugh <joychugh@gmail.com>
2015-08-02 02:36:53 -0400

    Stream friendly, Using guavaMurmurHash, trying to make it thread-safe.

--- src/main/java/com/jchugh/hyperloglog/Cardinality.java
+++ src/main/java/com/jchugh/hyperloglog/Cardinality.java
@@ -1,83 +1,124 @@
 package com.jchugh.hyperloglog;
 
-import com.google.common.hash.HashCode;
 import com.google.common.hash.Hashing;
-import com.google.common.hash.HashingInputStream;
-import com.jchugh.hyperloglog.hashing.MurmurHash3;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.charset.Charset;
 
 /**
  * @author jchugh
- * This does not work right now,
- * When it will, There will be a test included.
+ * Based on the HyperLogLog algorithm. Computes cardinality with error less than 1.04/sqrt(pow(2, logRegisterCount)).
+ * The register size is fixed at 8 bits for ease of implementation.
  */
 public class Cardinality {
-    private final List<Byte> registers;
+    private final int SEED = 1;
+    private final Object lock = new Object();
+    private byte[] registers;
     private final int logRegisterCount;
     private final int registerCount;
     private final double correctionConstant;
+    private final int shift;
     private int zeroValueRegisters;
 
+    /**
+     * This class and all it's methods except {@link #getCardinalityAsync()} are {@link net.jcip.annotations.ThreadSafe}
+     * @param logRegisterCount the log2 of Number of registers you wish to use, eg 16 registers = 4 logRegisterCount (log2(16) = 4).
+     *                         This value has to be between 4 and 16 (min and max inclusive).
+     */
     public Cardinality(int logRegisterCount) {
         if (!verifynumRegistersExponent(logRegisterCount)) {
             throw new IllegalArgumentException("4 <= logRegisterCount <= 16");
         }
         this.registerCount = 1 << logRegisterCount;
-        this.registers = new ArrayList<>(this.registerCount);
+        this.registers = new byte[this.registerCount];
         this.logRegisterCount = logRegisterCount;
         this.correctionConstant = getCorrectionConstant(this.registerCount);
+        this.shift = Integer.SIZE - logRegisterCount;
         zeroValueRegisters = 0;
     }
 
+    /**
+     * Submit a {@code long} input
+     * @param input the long input
+     */
     public void offer(long input) {
-        int x = Hashing.murmur3_32().hashLong(input).asInt();
-        int shift = Integer.SIZE - logRegisterCount;
-        int j = x >>> shift;
-        int w = (x << logRegisterCount) | (1 << (logRegisterCount - 1));
-        registers.add((byte) Math.max(registers.get(j), Integer.numberOfLeadingZeros(w) + 1));
-
+        int x = Hashing.murmur3_32(SEED).hashLong(input).asInt();
+        addToRegister(x);
     }
 
-    public void offer(String input) {
-
+    /**
+     * Submit a {@link String} input with {@link Charset}
+     * @param input the String input
+     * @param charset the Charset
+     */
+    public void offer(String input, Charset charset) {
+        int x = Hashing.murmur3_32(SEED).hashString(input, charset).asInt();
+        addToRegister(x);
     }
 
+    /**
+     * Submit a {@code byte[]} input. You can use {@link Cardinality#offer(String, Charset)} for Strings.
+     * @param input the byte[] input
+     */
     public void offer(byte[] input) {
-
+        int x = Hashing.murmur3_32(SEED).hashBytes(input).asInt();
+        addToRegister(x);
     }
 
-
-    public static long getCardinality(final List<byte[]> input, final int numRegistersExponent) {
-        if (!verifynumRegistersExponent(numRegistersExponent)) {
-            throw new IllegalArgumentException("4 <= numRegistersExponent <= 16");
+    private void addToRegister(final int hash) {
+        int j = hash >>> this.shift;
+        // So that we do not get more Leading Zeros than the remaining number of bits after we have calculated j
+        // and the rest of the hash value is 0. Thus max leading zeros == this.shift
+        int w = (hash << logRegisterCount) | (1 << (logRegisterCount - 1));
+        synchronized (lock) {
+            registers[j] = ((byte) Math.max(registers[j], Integer.numberOfLeadingZeros(w) + 1));
         }
-        final int numRegisters = 1 << numRegistersExponent;
-        // for simplicity of implementation, bits per register = 1 byte = 8
-        final double correctionConstant = getCorrectionConstant(numRegisters);
-        byte[] registers = new byte[numRegisters];
+    }
+
+    /**
+     * Get the cardinality of the inputs received so far.
+     * While the cardinality is calculated, no more values can be added.
+     * that snapshot. Useful when cardinality is needed less often, but slows down the system if getCardinality()
+     * is called a lot. Check {@link #getCardinalityAsync()}
+     * @return cardinality
+     */
+    public long getCardinalitySync() {
+        synchronized (lock) {
+            return getCardinality();
+        }
+    }
+
+    /**
+     * <B>Use With Caution</B> this method is {@link net.jcip.annotations.NotThreadSafe}
+     * Get the cardinality of the inputs received so far. Async.
+     * While the cardinality is calculated, you can still add data to the registers, this may result in slightly skewed
+     * cardinality for the instance it was called.
+     * @return cardinality of the items seen so far
+     */
+    public long getCardinalityAsync() {
+        return getCardinality();
+    }
+
+    private long getCardinality() {
         int zeroRegisterCount = 0;
-
-        for (byte[] v: input) {
-            int x = MurmurHash3.murmurhash3_x86_32(v, 0, v.length, 0);
-            int shift = Integer.SIZE - numRegistersExponent;
-            int j = x >>> shift;
-            // To prevent  0 hash value giving 32 leading zeros
-            int w = (x << numRegistersExponent) | (1 << (numRegistersExponent - 1));
-            registers[j] = (byte) Math.max(registers[j], Integer.numberOfLeadingZeros(w) + 1);
-        }
-
         double indicator = 0.0;
-        for (int i=0; i < numRegisters; ++i) {
+        for (int i = 0; i < registerCount; ++i) {
             // 2 ^ -register[i]
             indicator += (1.0 / (1 << registers[i]));
             if (registers[i] == 0) zeroRegisterCount++;
         }
-        indicator = 1/indicator;
-        double E = correctionConstant * numRegisters * numRegisters * indicator;
+        indicator = 1 / indicator;
+        double E = correctionConstant * registerCount * registerCount * indicator;
+        return Cardinality.correct(E, registerCount, zeroRegisterCount);
+    }
 
-        return Cardinality.correct(E, numRegisters, zeroRegisterCount);
+    /**
+     * Reset all the registers to 0.
+     * Creates a new list of registers, leaving previous one to be gc'd.
+     */
+    public void reset() {
+        synchronized (lock) {
+            this.registers = new byte[this.registerCount];
+        }
     }
 
     private static boolean verifynumRegistersExponent(int numRegisterExponent) {
@@ -98,18 +139,18 @@ public class Cardinality {
         }
     }
 
-    private static long correct(double E, int numOfRegisters, int emptyRegisters) {
-        if (E <= (5.0/2.0) * numOfRegisters) {
+    private static long correct(final double E, final int numOfRegisters, final int emptyRegisters) {
+        if (E <= (5.0 / 2.0) * numOfRegisters) {
             if (emptyRegisters != 0) {
-                return Math.round(numOfRegisters * Math.log(numOfRegisters/Double.valueOf(emptyRegisters)));
+                return Math.round(numOfRegisters * Math.log(numOfRegisters / Double.valueOf(emptyRegisters)));
             } else {
                 return Double.valueOf(E).longValue();
             }
         }
-        if (E <= (1.0/30.0) * (1l << 32)) {
+        if (E <= (1.0 / 30.0) * (1l << 32)) {
             return Double.valueOf(E).longValue();
         } else {
-            return Math.round(-(1l << 32) * Math.log(1 - E/(1l << 32)));
+            return Math.round(-(1l << 32) * Math.log(1 - E / (1l << 32)));
         }
     }
 }
